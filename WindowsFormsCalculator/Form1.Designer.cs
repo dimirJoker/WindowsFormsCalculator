912af33076e99d5306a79e823cc5ba56f8c6327a
dimirJoker <mucnikdmitrij14@gmail.com>
2022-06-11 00:44:03 +0300

    Add project files.

---
+++ WindowsFormsCalculator/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace WindowsFormsCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelValue1 = new System.Windows.Forms.Label();
+            this.labelValue2 = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.textBoxValue1 = new System.Windows.Forms.TextBox();
+            this.textBoxValue2 = new System.Windows.Forms.TextBox();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.buttonPlus = new System.Windows.Forms.Button();
+            this.buttonMinus = new System.Windows.Forms.Button();
+            this.buttonMulti = new System.Windows.Forms.Button();
+            this.buttonDivision = new System.Windows.Forms.Button();
+            this.labelError = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelValue1
+            // 
+            this.labelValue1.AutoSize = true;
+            this.labelValue1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelValue1.Location = new System.Drawing.Point(12, 9);
+            this.labelValue1.Name = "labelValue1";
+            this.labelValue1.Size = new System.Drawing.Size(64, 25);
+            this.labelValue1.TabIndex = 0;
+            this.labelValue1.Text = "value";
+            // 
+            // labelValue2
+            // 
+            this.labelValue2.AutoSize = true;
+            this.labelValue2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelValue2.Location = new System.Drawing.Point(12, 34);
+            this.labelValue2.Name = "labelValue2";
+            this.labelValue2.Size = new System.Drawing.Size(64, 25);
+            this.labelValue2.TabIndex = 1;
+            this.labelValue2.Text = "value";
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelResult.Location = new System.Drawing.Point(12, 59);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(65, 25);
+            this.labelResult.TabIndex = 2;
+            this.labelResult.Text = "result";
+            // 
+            // textBoxValue1
+            // 
+            this.textBoxValue1.Location = new System.Drawing.Point(82, 14);
+            this.textBoxValue1.Name = "textBoxValue1";
+            this.textBoxValue1.Size = new System.Drawing.Size(100, 20);
+            this.textBoxValue1.TabIndex = 3;
+            // 
+            // textBoxValue2
+            // 
+            this.textBoxValue2.Location = new System.Drawing.Point(82, 39);
+            this.textBoxValue2.Name = "textBoxValue2";
+            this.textBoxValue2.Size = new System.Drawing.Size(100, 20);
+            this.textBoxValue2.TabIndex = 4;
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Location = new System.Drawing.Point(82, 64);
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.Size = new System.Drawing.Size(100, 20);
+            this.textBoxResult.TabIndex = 5;
+            // 
+            // buttonPlus
+            // 
+            this.buttonPlus.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonPlus.Location = new System.Drawing.Point(188, 14);
+            this.buttonPlus.Name = "buttonPlus";
+            this.buttonPlus.Size = new System.Drawing.Size(45, 45);
+            this.buttonPlus.TabIndex = 6;
+            this.buttonPlus.Text = "+";
+            this.buttonPlus.UseVisualStyleBackColor = true;
+            this.buttonPlus.Click += new System.EventHandler(this.buttonPlus_Click);
+            // 
+            // buttonMinus
+            // 
+            this.buttonMinus.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonMinus.Location = new System.Drawing.Point(239, 14);
+            this.buttonMinus.Name = "buttonMinus";
+            this.buttonMinus.Size = new System.Drawing.Size(45, 45);
+            this.buttonMinus.TabIndex = 7;
+            this.buttonMinus.Text = "-";
+            this.buttonMinus.UseVisualStyleBackColor = true;
+            this.buttonMinus.Click += new System.EventHandler(this.buttonMinus_Click);
+            // 
+            // buttonMulti
+            // 
+            this.buttonMulti.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonMulti.Location = new System.Drawing.Point(188, 65);
+            this.buttonMulti.Name = "buttonMulti";
+            this.buttonMulti.Size = new System.Drawing.Size(45, 45);
+            this.buttonMulti.TabIndex = 8;
+            this.buttonMulti.Text = "*";
+            this.buttonMulti.UseVisualStyleBackColor = true;
+            this.buttonMulti.Click += new System.EventHandler(this.buttonMulti_Click);
+            // 
+            // buttonDivision
+            // 
+            this.buttonDivision.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonDivision.Location = new System.Drawing.Point(239, 65);
+            this.buttonDivision.Name = "buttonDivision";
+            this.buttonDivision.Size = new System.Drawing.Size(45, 45);
+            this.buttonDivision.TabIndex = 9;
+            this.buttonDivision.Text = "/";
+            this.buttonDivision.UseVisualStyleBackColor = true;
+            this.buttonDivision.Click += new System.EventHandler(this.buttonDivision_Click);
+            // 
+            // labelError
+            // 
+            this.labelError.AutoSize = true;
+            this.labelError.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelError.Location = new System.Drawing.Point(12, 87);
+            this.labelError.Name = "labelError";
+            this.labelError.Size = new System.Drawing.Size(0, 25);
+            this.labelError.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 379);
+            this.Controls.Add(this.labelError);
+            this.Controls.Add(this.buttonDivision);
+            this.Controls.Add(this.buttonMulti);
+            this.Controls.Add(this.buttonMinus);
+            this.Controls.Add(this.buttonPlus);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.textBoxValue2);
+            this.Controls.Add(this.textBoxValue1);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.labelValue2);
+            this.Controls.Add(this.labelValue1);
+            this.Name = "Form1";
+            this.Text = "Simple calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelValue1;
+        private System.Windows.Forms.Label labelValue2;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.TextBox textBoxValue1;
+        private System.Windows.Forms.TextBox textBoxValue2;
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.Button buttonPlus;
+        private System.Windows.Forms.Button buttonMinus;
+        private System.Windows.Forms.Button buttonMulti;
+        private System.Windows.Forms.Button buttonDivision;
+        private System.Windows.Forms.Label labelError;
+    }
+}
+
---
+++ WindowsFormsCalculator/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace WindowsFormsCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonPlus_Click(object sender, EventArgs e)
+        {
+            var ci = (CultureInfo)CultureInfo.CurrentCulture.Clone();
+            ci.NumberFormat.NumberDecimalSeparator = ".";
+            try
+            {
+                var value1 = Convert.ToDouble(textBoxValue1.Text,ci);
+                var value2 = Convert.ToDouble(textBoxValue2.Text,ci);
+                var result = value1 + value2;
+                textBoxResult.Text = result.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Invalid input!");
+            }
+        }
+
+        private void buttonMinus_Click(object sender, EventArgs e)
+        {
+            var ci = (CultureInfo)CultureInfo.CurrentCulture.Clone();
+            ci.NumberFormat.NumberDecimalSeparator = ".";
+            try
+            {
+                var value1 = Convert.ToDouble(textBoxValue1.Text, ci);
+                var value2 = Convert.ToDouble(textBoxValue2.Text, ci);
+                var result = value1 - value2;
+                textBoxResult.Text = result.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Invalid input!");
+            }
+        }
+
+        private void buttonMulti_Click(object sender, EventArgs e)
+        {
+            var ci = (CultureInfo)CultureInfo.CurrentCulture.Clone();
+            ci.NumberFormat.NumberDecimalSeparator = ".";
+            try
+            {
+                var value1 = Convert.ToDouble(textBoxValue1.Text, ci);
+                var value2 = Convert.ToDouble(textBoxValue2.Text, ci);
+                var result = value1 * value2;
+                textBoxResult.Text = result.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Invalid input!");
+            }
+        }
+
+        private void buttonDivision_Click(object sender, EventArgs e)
+        {
+            var ci = (CultureInfo)CultureInfo.CurrentCulture.Clone();
+            ci.NumberFormat.NumberDecimalSeparator = ".";
+            try
+            {
+                var value1 = Convert.ToDouble(textBoxValue1.Text, ci);
+                var value2 = Convert.ToDouble(textBoxValue2.Text, ci);
+                var result = value1 / value2;
+                textBoxResult.Text = result.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Invalid input!");
+            }
+        }
+    }
+}
